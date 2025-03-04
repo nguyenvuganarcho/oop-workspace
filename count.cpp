5bc8cc2a6b5c21a797cb07f0dfd86de65ee70204
nguyenvuganarcho <vukhoinguyen204@gmail.com>
2025-03-04 15:49:19 +1030

    add 2 files

---
+++ count.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int count(int arr[], int n) {
+    int total = 0;
+    
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            total++;
+        }
+    }
+
+    return 0;
+}
