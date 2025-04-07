beedb25aab4a8ba3ff94e9cbf6dd46e4e68fa640
nguyenvuganarcho <vukhoinguyen204@gmail.com>
2025-04-07 14:18:03 +0930

    Update main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
-#include "Musician.h"
+#include "player.h"
+#include "wizard.h"
+#include "warrior.h"
+
+using namespace std;
 
 int main() {
-    Musician m1;
-    std::cout << "Default instrument: " << m1.get_instrument() << ", Default experience: " << m1.get_experience() << std::endl;
-
-    Musician m2("guitar", 5);
-    std::cout << "Instrument: " << m2.get_instrument() << ", Experience: " << m2.get_experience() << std::endl;
-
+    //Wizard(name, health, damage, mana)
+    Wizard wizard("Gandalf", 100, 20, 50);
+    //Warrior(name, health, damage, weapon)
+    Warrior warrior("Aragorn", 120, 25, "Sword");
+    
+    cout << "Let the battle begin!" << endl;
+    
+    while (wizard.getHealth() > 0 && warrior.getHealth() > 0) {
+        wizard.castSpell(&warrior);
+        if (warrior.getHealth() > 0) {
+            warrior.swingWeapon(&wizard);
+        }
+    }
+    
+    if (wizard.getHealth() > 0) {
+        cout << wizard.getName() << " wins!" << endl;
+    } else {
+        cout << warrior.getName() << " wins!" << endl;
+    }
+    
     return 0;
 }
